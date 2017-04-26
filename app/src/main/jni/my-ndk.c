bb2ae8f1c617306d3ae7203660216e21d656df81
yanqiang <yan--qiang@sohu.com>
2017-04-26 15:21:10 +0800

    NDK

---
+++ app/src/main/jni/my-ndk.c
@@ -0,0 +1,36 @@
+#include <jni.h>
+#include <errno.h>
+
+ JNIEXPORT jint JNICALL Java_com_appwoo_txtw_theme_deepblack_Load_addInt (JNIEnv * env, jobject obj, jint a, jint b) {
+     return a + b;
+ }
+
+ JNIEXPORT jstring JNICALL Java_com_appwoo_txtw_theme_deepblack_Load_Mkdir (JNIEnv * env, jobject obj) {
+
+	 int nTmp =  mkdir("/data/data/com.browser.txtw/files/yanqiang");
+
+	 if(0 != nTmp)
+	 {
+		 nTmp = errno ;
+		 return (*env)->NewStringUTF(env,strerror(errno) );
+	 }
+
+	 return (*env)->NewStringUTF(env, "ok");
+ }
+
+ JNIEXPORT jstring JNICALL Java_com_appwoo_txtw_theme_deepblack_Load_Mount (JNIEnv * env, jobject obj) {
+
+	 int nTmp =  execl("/system/bin/mount", "mount", "-o", "remount", "/system", (char *) 0);
+
+	 if(0 != nTmp)
+	 {
+		 nTmp = errno ;
+		 return (*env)->NewStringUTF(env,strerror(errno) );
+	 }
+
+	 return (*env)->NewStringUTF(env, "ok");
+ }
+
+
+
+
