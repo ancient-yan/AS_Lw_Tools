6697e49b04aca1ce8bdc48046f6b52f50a19df69
yanqiang <yan--qiang@sohu.com>
2017-06-01 17:27:07 +0800

    tag

--- app/src/main/java/com/appwoo/txtw/theme/deepblack/PackageVerifyReceiver.java
+++ app/src/main/java/com/appwoo/txtw/theme/deepblack/PackageVerifyReceiver.java
@@ -7,7 +7,7 @@ import android.content.pm.PackageManager;
 import android.util.Log;
 
 public class PackageVerifyReceiver extends BroadcastReceiver {
-    private static final String Tag = PackageVerifyReceiver.class.getSimpleName();
+    private static final  String Tag = "my_log";
     private final String PackageVerificationResultAction = "android.intent.action.package_verification.result";
 
     public void onReceive(Context context, Intent intent) {
@@ -24,6 +24,9 @@ public class PackageVerifyReceiver extends BroadcastReceiver {
             intentLw.putExtra("android.content.pm.extra.VERIFICATION_ID", verifiedId);
             intentLw.putExtra("android.content.pm.extra.VERIFICATION_PACKAGE_NAME", packageName);
             intentLw.putExtra("apk_install_path", intent.getDataString());
+
+            Log.e(Tag, "apk_install_path : " + intent.getDataString() );
+
             context.sendBroadcast(intentLw);
             Log.v(Tag, "PackageVerifyReceiver android5.1.1 verifiedId=android.content.pm.extra.VERIFICATION_ID   packageName=android.content.pm.extra.VERIFICATION_PACKAGE_NAME");
         }
