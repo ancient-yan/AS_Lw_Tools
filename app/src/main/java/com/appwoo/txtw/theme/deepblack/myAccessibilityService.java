62d7152647fddcfba1ea6903819b261ccf9e7308
yanqiang <yan--qiang@sohu.com>
2017-12-18 15:43:50 +0800

    GLOBAL_ACTION_BACK

--- app/src/main/java/com/appwoo/txtw/theme/deepblack/myAccessibilityService.java
+++ app/src/main/java/com/appwoo/txtw/theme/deepblack/myAccessibilityService.java
@@ -3,6 +3,7 @@ package com.appwoo.txtw.theme.deepblack;
 import android.accessibilityservice.AccessibilityService;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class myAccessibilityService extends AccessibilityService {
     private static final  String TAG = "my_log";
@@ -13,8 +14,17 @@ public class myAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG, "getPackageName : " + event.getPackageName() );
-        Log.e(TAG, "getAction : " + event.getAction() );
+        String strPackageName = "" + event.getPackageName();
+        Log.e(TAG, "getPackageName : " +  strPackageName);
         Log.e(TAG, AccessibilityEvent.eventTypeToString(event.getEventType() ) );
+
+        AccessibilityNodeInfo nodeInfo = event.getSource();
+        if(null != nodeInfo)
+            Log.e(TAG, "getClassName : " + nodeInfo.getClassName() );
+
+        if("com.mediatek.filemanager".equals(strPackageName) )
+        {
+            performGlobalAction(GLOBAL_ACTION_BACK);
+        }
     }
 }
