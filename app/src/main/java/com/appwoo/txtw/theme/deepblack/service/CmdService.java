cde5a41b02b44163be5efb1183296b17fc838132
yanqiang <yan--qiang@sohu.com>
2019-06-10 16:16:52 +0800

    GetTopService

--- app/src/main/java/com/appwoo/txtw/theme/deepblack/service/CmdService.java
+++ app/src/main/java/com/appwoo/txtw/theme/deepblack/service/CmdService.java
@@ -1,10 +1,15 @@
 package com.appwoo.txtw.theme.deepblack.service;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class CmdService extends Service {
+    private static final  String TAG = "my_log";
+
     public CmdService() {
     }
 
@@ -12,5 +17,25 @@ public class CmdService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "CmdService.onStartCommand");
+
+        int nCmd = intent.getIntExtra("cmd", -1);
+        Log.e(TAG, "CmdService.onStartCommand.nCmd : " + nCmd);
+
+        switch (nCmd) {
+            case 1: {
+                ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+                String runningActivity = activityManager.getRunningTasks(1).get(0).topActivity.getClassName();
+
+                Log.d(TAG, "runningActivity : " + runningActivity);
+            }
+            break;
+        }
+
+        return super.onStartCommand(intent, flags, startId);
     }
 }
