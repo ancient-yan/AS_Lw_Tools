89bab05890aeb1c462307b8aae53ea6d6d7bafcd
yanqiang <yan--qiang@sohu.com>
2019-06-10 16:09:42 +0800

    CmdService

---
+++ app/src/main/java/com/appwoo/txtw/theme/deepblack/service/CmdService.java
@@ -0,0 +1,16 @@
+package com.appwoo.txtw.theme.deepblack.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class CmdService extends Service {
+    public CmdService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
